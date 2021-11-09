53670d272659631a0a108ba21d09ffb3f4b0b0ac
Pierre Zemb <pierre.zemb@clever-cloud.com>
2021-11-09 16:06:45 +0100

    feat(all): add support for api 700

--- foundationdb-sys/build.rs
+++ foundationdb-sys/build.rs
@@ -24,6 +24,8 @@ const INCLUDE_PATH: &str = "-I./include/600";
 const INCLUDE_PATH: &str = "-I./include/610";
 #[cfg(all(feature = "embedded-fdb-include", feature = "fdb-6_2"))]
 const INCLUDE_PATH: &str = "-I./include/620";
+#[cfg(all(feature = "embedded-fdb-include", feature = "fdb-7_0"))]
+const INCLUDE_PATH: &str = "-I./include/700";
 
 fn main() {
     // Link against fdb_c.
@@ -62,6 +64,10 @@ fn main() {
     #[cfg(feature = "fdb-6_2")]
     {
         api_version = 620;
+    }
+    #[cfg(feature = "fdb-7_0")]
+    {
+        api_version = 700;
     }
 
     // Sigh, bindgen only takes a String for its header path, but that's UTF-8 while
